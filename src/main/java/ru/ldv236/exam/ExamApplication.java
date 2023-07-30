e445de746e45d5dbeb9c5bbde6f3803cd0f46409
Ldv236 <Ldv236@yandex.ru>
2023-07-30 13:42:17 +0200

    swagger

--- src/main/java/ru/ldv236/exam/ExamApplication.java
+++ src/main/java/ru/ldv236/exam/ExamApplication.java
@@ -1,9 +1,11 @@
 package ru.ldv236.exam;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ExamApplication {
 
 	public static void main(String[] args) {
